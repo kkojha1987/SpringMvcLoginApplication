bbe156a027ddf848cefb3190aa6312e1c614db12
kanhaiya ojha <kkojha1987@gmail.com>
2017-08-11 13:59:57 +0530

    Some Annotation Used like @PathVariable and @RequestParam

--- WebContent/WEB-INF/hello.jsp
+++ WebContent/WEB-INF/hello.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="s" uri="http://www.springframework.org/tags"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -9,5 +10,16 @@
 <body>
 <h3>Welcome</h3>
 <p>${appname}</p>
+<br/>
+<p>Value of URL is:- ${val}</p>
+<form action="welcome" method="post">
+<input type="text" name="firstName">
+<br/>
+<input type="text" name="lastName">
+<br/>
+<input type="submit" value="Submit">
+</form>
+<br/>
+<p>Your Name:- ${fname} ${lname}</p>
 </body>
 </html>
--- src/com/java/controllers/MyController.java
+++ src/com/java/controllers/MyController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.java.model.Account;
@@ -19,11 +21,22 @@ public class MyController {
 	{
 		return "hello";
 	}
-	@RequestMapping("/welcome")
-	public ModelAndView modelMethod()
+	@RequestMapping(value="/welcome/name/{a}", method=RequestMethod.GET)
+	public ModelAndView modelMethod(@PathVariable("a") String val)
 	{
 		ModelAndView model=new ModelAndView("hello");
 		model.addObject("appname", "WELCOME TO WEB PAGE");
+		model.addObject("val", val);
+		return model;
+	}
+	@RequestMapping(value="/welcome/name/{a}", method=RequestMethod.POST)
+	public ModelAndView modelMethod(@PathVariable("a") String val, @RequestParam("firstName") String fname, @RequestParam("lastName") String lname)
+	{
+		ModelAndView model=new ModelAndView("hello");
+		model.addObject("appname", "WELCOME TO WEB PAGE");
+		model.addObject("val", val);
+		model.addObject("fname", fname);
+		model.addObject("lname", lname);
 		return model;
 	}
 	@RequestMapping(value="login", method=RequestMethod.GET)
